7db726a8f6490fd98df8f38e7ce9b5ff2d21fbc4
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-09-06 22:11:37 -0500

    implementando el estilo del componente progress spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AppComponent } from './app.component';
+import { ProgressSpinnerComponent } from './progress-spinner/progress-spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressSpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/progress-spinner/progress-spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+@Component({
+  selector: 'app-progress-spinner',
+  templateUrl: './progress-spinner.component.html',
+  styleUrls: ['./progress-spinner.component.scss']
+})
+export class ProgressSpinnerComponent implements OnInit {
+  defaultColor: ThemePalette = 'accent';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
